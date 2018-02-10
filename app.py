d2be83e9bea74bbc762509233a202eeda14042f9
jeimer398 <josheimer98@gmail.com>
2018-02-10 13:42:44 -0500

    python connects to html but not css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import sys
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/home/jeimer398/Documents/HackBU/Spring2018/HackBU2018/site")
 
 @app.route("/", methods = ["GET", "POST"])
-def display():
-    return "Working"
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
