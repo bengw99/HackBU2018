4f84beca1d66c6b939c1925cafe835366e107cf0
jeimer398 <josheimer98@gmail.com>
2018-02-10 17:48:20 -0500

    Progress (generates offlink but 404 error)

---
+++ auth.py
@@ -0,0 +1,22 @@
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+
+def start_auth():
+    # Creates a flow
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+                'client_secret.json',
+                scopes=['https://www.googleapis.com/auth/gmail'],
+                redirect_uri = ['http://localhost:5000/email.html'])
+
+    # Tells user where to go
+    auth_url, _ = flow.authorization_url(prompt="consent")
+
+    print("Please go to this URL: {}".format(auth_url))
+
+    # Provides user with authorization code and then an access token
+    code = input("Enter the authorization code: ")
+    flow.fetch_token(code = code)
+
+    # Use flow.authorized_session
+    session = flow.authorized_session()
+    print(session.get("https://googleapis.com/userinfo/v2/me").json())
