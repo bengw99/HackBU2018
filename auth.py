178a0f4813097fb78c94fd097a01c5172e24c3dc
jeimer398 <josheimer98@gmail.com>
2018-02-10 22:50:31 -0500

    class working

--- auth.py
+++ auth.py
@@ -2,21 +2,24 @@ import google.oauth2.credentials
 import google_auth_oauthlib.flow
 
 class Auther():
-	#flow
-	#auth_url
+	flow = None 
+	auth_url = None 
 	
-	def __init__():
+	def __init__(self):
 		# Creates a flow
-		flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+		self.flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
 					'client_secret.json',
 					scopes=['https://mail.google.com'],
 					redirect_uri = 'http://localhost:5000/email/')
 
 		# Tells user where to go
-		auth_url, _ = flow.authorization_url(prompt="consent")
+		self.auth_url, _ = self.flow.authorization_url(prompt="consent")
 		
-	def get_auth_url():
-		return auth_url
+	def get_auth_url(self):
+		return self.auth_url
+
+        def get_flow(self):
+            return self.flow
 		
 def make_auther():
 	auther = Auther()
