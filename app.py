922ed186db86eb1e849adf46a66f96ab8efa052b
Matthew Wilomovsky <mwilomo1@binghamton.edu>
2018-02-10 19:31:23 -0500

    index updates

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ import sys
 
 #app = Flask(__name__, template_folder="/home/jeimer398/Documents/HackBU/Spring2018/HackBU2018/site")
 
-app = Flask(__name__, template_folder="../HackBU2018/site")
+app = Flask(__name__, static_folder='static', static_url_path='/static', template_folder="../HackBU2018/site")
 
 @app.route("/", methods = ["GET", "POST"])
 def home():
     return render_template("index.html")
+
+@app.route('/contact.php')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
