406c7d4cebb8594cb32734ed57e6bc44e17cfbd8
Ben Weiss <bengw99@gmail.com>
2018-02-10 19:44:30 -0500

    trying to make button run python

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from auth import *
 
 app = Flask(__name__, static_folder='static', static_url_path='/static', template_folder="../HackBU2018/site")
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
 def home():
-    print(request.method, request.method == "POST")
-    if request.method == "POST":
-        start_auth()
     return render_template("index.html")
 
 @app.route('/contact.php')
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
-
+@app.route('/preauth/')
+def preauth():
+	start_auth()
+	return
 
 
 
