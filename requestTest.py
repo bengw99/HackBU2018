c738d083b38e3d8bdb4573d3b06c502f62823c64
tsteige1 <tsteiger@rochester.rr.com>
2018-02-11 07:11:54 -0500

    Check safety of a website

---
+++ requestTest.py
@@ -0,0 +1,31 @@
+
+import requests
+from requests.auth import HTTPBasicAuth
+import certifi
+def main():
+	checkURL("http://www.bittorrent.com")
+	
+def checkURL(givenURL):
+	
+	try:
+		r = requests.get(givenURL, verify=True)
+		print("Response code: %d" % r.status_code)
+		if(r.status_code == requests.codes.ok):
+			print("Website works")
+		else:
+			print("Website does not work")
+		print("Website is certified")
+		
+	except:
+		print("Shit is not secure, yo")
+		
+	try:
+		
+		r2 = requests.get(givenURL, auth=HTTPBasicAuth('user', 'pass'))
+
+		print("Website does have basic HTTP Authentication")
+	except:
+		print("Website does not have basic HTTP Authentication")
+	
+if __name__ == "__main__":
+	main()
