108f9a8dd909fb83c7d5297934ed3f400a6fdd84
Matthew Wilomovsky <mwilomo1@binghamton.edu>
2018-02-11 04:21:16 -0500

    site stuff

--- site/contact.php
+++ site/contact.php
@@ -1,2 +1,13 @@
 <?php
+	$myfile=fopen("contact.txt",w) or die("Unable to open file!");
+	$txt = $GET_["name"];
+	fwrite($myfile,$txt\t);
+	$txt= $GET_["email"];
+	fwrite($myfile,$txt\t);
+	$txt= $GET_["phone"];
+	fwrite($myfile,$txt: );
+	$txt= $GET_["subject"];
+	fwrite($myfile,$txt\n);
+	$txt= $GET_["message"];
+	fclose($myfile);
 	?>
