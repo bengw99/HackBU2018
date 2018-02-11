5333124bb36033a4d7a8d13f7c2f46bd3c066e7d
jeimer398 <josheimer98@gmail.com>
2018-02-11 12:39:40 -0500

    almost up to par

--- batchrate.py
+++ batchrate.py
@@ -1,3 +1,5 @@
+from requestTest import *
+
 def batch_rate(messages):
     for message in messages:
-        message.set_safety_rating(0)
+        message.set_safety_rating(checkURL(message.get_url()))
