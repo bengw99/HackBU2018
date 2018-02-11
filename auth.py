92602187a36b4298b1807157f9a1724f43b11156
jeimer398 <josheimer98@gmail.com>
2018-02-10 21:49:37 -0500

    OMG IT KINDA WORKS

--- auth.py
+++ auth.py
@@ -5,13 +5,14 @@ def start_auth():
     # Creates a flow
     flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
                 'client_secret.json',
-                scopes=['https://www.googleapis.com/auth/gmail'],
+                scopes=['https://mail.google.com'],
                 redirect_uri = 'http://localhost:5000/email/')
 
     # Tells user where to go
     auth_url, _ = flow.authorization_url(prompt="consent")
 
     print("Please go to this URL: {}".format(auth_url))
+
 
     # Provides user with authorization code and then an access token
     code = input("Enter the authorization code: ")
