a9fe0ce8d7f691c051e5b004945e54e743e27706
jeimer398 <josheimer98@gmail.com>
2018-02-11 00:18:31 -0500

    WE GOT THE CODEEEEE

--- app.py
+++ app.py
@@ -7,24 +7,28 @@ app = Flask(__name__, static_folder='static', static_url_path='/static', templat
 
 @app.route("/")
 def home():
+    print("home")
     return render_template("index.html")
 
 @app.route('/contact.php')
 def static_from_root():
+    print("static")
     return send_from_directory(app.static_folder, request.path[1:])
 
 @app.route('/preauth/')
 def preauth():
+    print("PREAUTH being called")
     auther = make_auther()
     auth_url = auther.get_auth_url()
     return redirect(auth_url)
 
-@app.route('/email/<any:pluscode>')
-def postauth(pluscode):
-    print("PLUSCODE = " + pluscode)
-    print("------------------------WORKING---------------------")
-    messages = scrape(pluscode)
-    print("___________---------------CHECK HERE FOR MESSAGES: " + messages)
+@app.route('/email/', defaults={'path': ''})
+def postauth(path):
+    code = request.args.get('code')
+    print("------POSTAUTH-----")
+    print("CODE = " + code)
+    messages = scrape(code)
+    print("---------------CHECK HERE FOR MESSAGES: " + messages)
     return messages
 
 if __name__ == "__main__":
